70e42f1a5cf0b3a9be25a536c5d100227b4c1068
dominic-myb <desguerra246@gmail.com>
2024-04-15 00:51:56 +0800

    added two enemies

---
+++ scripts/eel.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+var player : CharacterBody2D
+var in_range : bool
+var speed := 100.0
+func _ready():
+	player = get_tree().get_first_node_in_group("Player")
+	$AnimationPlayer.play("idle")
+func _process(_delta):
+	if in_range:
+		$AnimationPlayer.play("move")
+	else:
+		$AnimationPlayer.play("idle")
+func _physics_process(_delta):
+	_follow_player()
+	move_and_slide()
+func _follow_player():
+	var direction = (player.global_position - self.global_position)
+	if in_range:
+		velocity = direction.normalized() * speed
+		_sprite_position(direction.normalized().x)
+	else:
+		velocity = Vector2.ZERO
+func _sprite_position(pos: float):
+	if pos > 0: $AnimatedSprite2D.flip_h = false
+	elif pos < 0: $AnimatedSprite2D.flip_h = true
+
+func _on_player_detection_body_entered(body):
+	if body.is_in_group("Player"):
+		in_range = true
+
+func _on_player_detection_body_exited(body):
+	if body.is_in_group("Player"):
+		in_range = false
