1ab84bd5a28e0d24e5fa9435822c1cb3d90024f9
dominic-myb <desguerra246@gmail.com>
2024-04-15 12:07:52 +0800

    Update player script

--- scripts/player_sea.gd
+++ scripts/player_sea.gd
@@ -2,15 +2,13 @@ extends CharacterBody2D
 class_name Player
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 const PROJECTILE_PATH = preload("res://projectiles/projectile_1.tscn")
-#can add other projectile
 #fix the projectile orientation
 #add attack buttons
-#add enemy damage
-#add the game and utils script
 
 @onready var joystick = $"../../CanvasLayer/HBoxContainer/Joystick"
 var speed = 600.0
 var is_moving: bool
+
 func _ready():
 	$AnimationPlayer.play("idle")
 
@@ -22,13 +20,14 @@ func _physics_process(delta):
 	_movement(direction, delta)
 	move_and_slide()
 
-func _process(delta):
+func _process(_delta):
 	_sprite_position(velocity.x)
 	if is_moving:
 		$AnimationPlayer.play("move")
 	else:
 		$AnimationPlayer.play("idle")
 		$CollisionShape2D.set_rotation_degrees(0)
+
 func _gravity(delta: float):
 	if not is_on_floor():
 		velocity.y += gravity * delta * 0.5
@@ -56,3 +55,13 @@ func player_attack(direction):
 	get_parent().add_child(projectile)
 	projectile.position = $Aim/Marker2D.global_position
 	projectile.velocity = direction
+
+func take_damage(damage):
+	Game.player_hp -= damage
+	if Game.player_hp <= 0: death()
+	else: return Game.player_hp
+
+func death():
+	$AnimationPlayer.play("death")
+	await $AnimationPlayer.animation_finished
+	queue_free()
