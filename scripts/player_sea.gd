6ee1f4f0fded458794f03878b265353b30fce304
dominic-myb <desguerra246@gmail.com>
2024-04-16 01:51:25 +0800

    Add rotation to the projectile

--- scripts/player_sea.gd
+++ scripts/player_sea.gd
@@ -2,26 +2,29 @@ extends CharacterBody2D
 class_name Player
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 const PROJECTILE_PATH = preload("res://projectiles/projectile_1.tscn")
-#fix the projectile orientation
-#add attack buttons
-
+@onready var player = %Player
 @onready var joystick = $"../../CanvasLayer/HBoxContainer/Joystick"
 var speed = 600.0
 var is_moving: bool
-
+var is_pressed = false
+var is_hurt := false
 func _ready():
 	$AnimationPlayer.play("idle")
 
 func _physics_process(delta):
 	var direction = joystick.vector_pos.normalized()
 	if Input.is_action_just_pressed("ui_accept"):
-		player_attack(direction)
+		player_attack()
 	_gravity(delta)
 	_movement(direction, delta)
 	move_and_slide()
 
 func _process(_delta):
 	_sprite_position(velocity.x)
+	if is_hurt:
+		$AnimationPlayer.play("hurt")
+		await $AnimationPlayer.animation_finished
+		is_hurt = false
 	if is_moving:
 		$AnimationPlayer.play("move")
 	else:
@@ -50,18 +53,23 @@ func _sprite_position(pos: float):
 		$AnimatedSprite2D.flip_h = true
 		$CollisionShape2D.set_rotation_degrees(-30)
 
-func player_attack(direction):
+func player_attack():
+	var direction = $Aim/Marker2D.global_position - self.position
 	var projectile = PROJECTILE_PATH.instantiate()
 	get_parent().add_child(projectile)
 	projectile.position = $Aim/Marker2D.global_position
-	projectile.velocity = direction
-
+	projectile.velocity = direction * speed
+	projectile.rotation = atan2(direction.y, direction.x)
+	
 func take_damage(damage):
+	is_hurt = true
 	Game.player_hp -= damage
+	print_debug(Game.player_hp)
+	
 	if Game.player_hp <= 0: death()
-	else: return Game.player_hp
 
 func death():
 	$AnimationPlayer.play("death")
 	await $AnimationPlayer.animation_finished
 	queue_free()
+
