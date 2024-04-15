6beb9f7019bd118def29462d36cdbea84eea33a9
dominic-myb <desguerra246@gmail.com>
2024-04-16 01:50:54 +0800

    Remove sprite flip

--- scripts/projectiles/projectile_1.gd
+++ scripts/projectiles/projectile_1.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 var speed := 300
 func _ready():
 	$AnimatedSprite2D.play("default")
+
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity.normalized() * delta * speed)
 	if collision_info:
@@ -9,6 +10,4 @@ func _physics_process(delta):
 		if collider.has_method("take_damage"):
 			collider.take_damage(Game.player_damage)
 		queue_free()
-func _process(delta):
-	if velocity.x > 0: $AnimatedSprite2D.flip_h = false
-	elif velocity.x < 0: $AnimatedSprite2D.flip_h = true
+
