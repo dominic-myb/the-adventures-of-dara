9353f50bb76f20040e270ba8f02d431bb215cd01
dominic-myb <desguerra246@gmail.com>
2024-04-15 12:07:32 +0800

    Update projectile script

--- scripts/projectiles/projectile_1.gd
+++ scripts/projectiles/projectile_1.gd
@@ -4,6 +4,11 @@ func _ready():
 	$AnimatedSprite2D.play("default")
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity.normalized() * delta * speed)
+	if collision_info:
+		var collider = collision_info.get_collider()
+		if collider.has_method("take_damage"):
+			collider.take_damage(Game.player_damage)
+		queue_free()
 func _process(delta):
 	if velocity.x > 0: $AnimatedSprite2D.flip_h = false
 	elif velocity.x < 0: $AnimatedSprite2D.flip_h = true
