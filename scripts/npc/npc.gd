ccf9f12ed6ee14db4047f63f6662b4382fd61868
Dominic Esguerra <109610236+dominic-myb@users.noreply.github.com>
2024-04-26 23:19:36 +0800

    Add enum on NPC (parent)

--- scripts/npc/npc.gd
+++ scripts/npc/npc.gd
@@ -1,14 +1,29 @@
 class_name NPC
 extends Node
 
+"""
++ NPC: init signals pressed, locked, unlocked, accept, done
++ Clam: init lines[string], pic[texture2d]
++ Quest_N: should handle the lines in that quest
++ first quest should be unlocked [edit_when]: we have game tutorial
++ other quest should be QUEST_STATE.LOCKED
++ Convert all english[btn, dialogue] to filipino
+"""
+
 signal pressed
-signal accepted
-signal done
-signal pending
+
+enum QUEST_STATE {
+	LOCKED,
+	UNLOCKED,
+	ACCEPTED,
+	DONE
+}
 
 var lines_counter : int = 0
 var lines : Array[String] = []
 var pictures : Array[Texture2D] = []
+
+var quest_status : int
 
 func controls(container1: HBoxContainer, container2: HBoxContainer, value: bool):
 	container1.visible = value
@@ -23,4 +38,4 @@ func interact_btn(btn: TouchScreenButton, value: bool):
 func line_controller(lines_holder: Label, img_holder: TextureRect):
 	img_holder.texture = pictures[lines_counter]
 	lines_holder.text = lines[lines_counter]
-	
+
