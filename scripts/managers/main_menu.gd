43544eab075cacb4b45fa0d869622002e4c12846
Dominic Esguerra <109610236+dominic-myb@users.noreply.github.com>
2024-05-06 23:55:44 +0800

    Moved Directory

--- scripts/managers/main_menu.gd
+++ scripts/managers/main_menu.gd
@@ -1,13 +1,17 @@
 extends Control
 
 var scene_path = "res://scenes/world_sea.tscn"
+var prelude = "res://scenes/prelude.tscn"
 
 func _ready():
 	Utils.saveGame()
 	Utils.loadGame()
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file(scene_path)
+	if Game.is_played:
+		get_tree().change_scene_to_file(scene_path)
+	else: 
+		get_tree().change_scene_to_file(prelude)
 
 func _on_options_pressed():
 	pass # Replace with function body.
